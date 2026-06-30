24eee168c17a8579e39d220d81b3bd47fe772e2c
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-06-30 16:32:11 +0200

    chore: migrate project to Spring Boot 3

--- src/main/java/com/wiktoriapilch/itassettracker/Main.java
+++ src/main/java/com/wiktoriapilch/itassettracker/Main.java
@@ -1,13 +1,11 @@
 package com.wiktoriapilch.itassettracker;
 
-import com.wiktoriapilch.itassettracker.devices.Device;
-import com.wiktoriapilch.itassettracker.devices.DeviceStatus;
-import com.wiktoriapilch.itassettracker.employees.Employee;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("IT Asset Tracker ### Starting");
-        Employee employee = new Employee("John", "Pat", "john.pat@google.com");
-        Device laptop = new Device("MacBook Pro", "SN-987114", DeviceStatus.AVAILABLE);
+        SpringApplication.run(Main.class, args);
     }
 }
