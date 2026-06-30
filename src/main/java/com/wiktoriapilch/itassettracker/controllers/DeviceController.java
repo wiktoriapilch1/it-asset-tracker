5dc8527185b61cffe784a538b9b0adad7f76a252
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-07-01 00:25:42 +0200

    feat: integrate database persistance into DeviceController

--- src/main/java/com/wiktoriapilch/itassettracker/controllers/DeviceController.java
+++ src/main/java/com/wiktoriapilch/itassettracker/controllers/DeviceController.java
@@ -1,27 +1,27 @@
 package com.wiktoriapilch.itassettracker.controllers;
 
 import com.wiktoriapilch.itassettracker.devices.Device;
-import com.wiktoriapilch.itassettracker.devices.DeviceStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.wiktoriapilch.itassettracker.repository.DeviceRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/devices")
 public class DeviceController {
+    private  final DeviceRepository deviceRepository;
 
-    @GetMapping("/api/devices")
-    public List<Device> getAllDevices() {
-        return List.of(
-                new Device("Dell Latitude", "SN123", DeviceStatus.AVAILABLE),
-                new Device("MacBook Pro", "SN456", DeviceStatus.ASSIGNED)
-        );
+    public DeviceController(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
     }
 
-    @PostMapping("/api/devices")
-    public String addDevice(@RequestBody Device device) {
-        return "created" + device.getName();
+    @GetMapping
+    public List<Device> getAllDevices() {
+        return deviceRepository.findAll();
+    }
+
+    @PostMapping
+    public Device addDevice(@RequestBody Device device) {
+        return deviceRepository.save(device);
     }
 }
