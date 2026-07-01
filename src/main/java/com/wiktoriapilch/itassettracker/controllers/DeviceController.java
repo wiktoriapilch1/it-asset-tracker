64f68de9d9c760f6fea4abcd5b5c25b94c1608f1
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-07-01 13:41:54 +0200

    feat: add PATCH ednpoint for Device

--- src/main/java/com/wiktoriapilch/itassettracker/controllers/DeviceController.java
+++ src/main/java/com/wiktoriapilch/itassettracker/controllers/DeviceController.java
@@ -5,6 +5,7 @@ import com.wiktoriapilch.itassettracker.repository.DeviceRepository;
 
 import jakarta.validation.Valid;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +32,21 @@ public class DeviceController {
     @DeleteMapping("/{id}")
     public void deleteDevice(@PathVariable Long id) {
         deviceRepository.deleteById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Device updateDevice(@PathVariable Long id, @RequestBody Device updates) {
+        Device device_db = deviceRepository.findById(id).orElseThrow();
+        if(StringUtils.hasText(updates.getName())) {
+            device_db.setName(updates.getName());
+        }
+        if(StringUtils.hasText(updates.getSerialNumber())) {
+            device_db.setSerialNumber(updates.getSerialNumber());
+        }
+        if (updates.getStatus() != null) {
+            device_db.setStatus(updates.getStatus());
+        }
+        deviceRepository.save(device_db);
+        return device_db;
     }
 }
