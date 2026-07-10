82894ae4d6f92e76f62c72fd9ff747d6a316b10a
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-07-10 15:17:25 +0200

    feat: add Security Filter Chain

--- src/main/java/com/wiktoriapilch/itassettracker/config/SecurityConfig.java
+++ src/main/java/com/wiktoriapilch/itassettracker/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.wiktoriapilch.itassettracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +15,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/login").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .build();
     }
 }
