e9af3c9c4b4a0c061d6f88988f5bc4a914307aaa
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-07-16 13:54:48 +0200

    feat: add getSignInKey() and generateToken() methods for JwtService

--- src/main/java/com/wiktoriapilch/itassettracker/services/JwtService.java
+++ src/main/java/com/wiktoriapilch/itassettracker/services/JwtService.java
@@ -1,7 +1,15 @@
 package com.wiktoriapilch.itassettracker.services;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
@@ -9,4 +17,18 @@ public class JwtService {
     private String secretKey;
     @Value("${security.jwt.expiration-time}")
     private Long jwtExpiration;
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
 }
